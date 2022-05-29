ec7fb66809b8d15ded9cd40932e529ace945f1a5
Petr <petr_mameev@mail.ru>
2022-05-29 14:36:05 +0700

    first commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int priceTicket = 119;
+        int priceMile = 20;
+        int mile = priceTicket / priceMile;
+        System.out.println("Стоимость вашего билета равна");
+        System.out.println(priceTicket + "рубль(ей)");
+        System.out.println("Количество начисленных миль за купленный билет:");
+        System.out.println(mile + "миль(и)");
+    }
+}
